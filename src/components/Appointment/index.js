4ce951da9f883bca7164fab7af0cb3c0d7570184
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-28 00:13:01 -0400

    Conditionally Rendering our First Components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
 import Header from "components/Appointment/Header";
@@ -7,19 +7,24 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={() => console.log("CONFIRM")}
-          onEdit={() => console.log("EDIT")}
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
+      <Empty onAdd={() => console.log("CREATE")} />
     </article>
   );
 }
