21db9fbd0bc5caec7333c6920bfb1366317b1a00
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-27 23:53:23 -0400

    Transition with Replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setMode(newMode);
-    setHistory(prev => [...prev, newMode])
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0, prev.length - 1), newMode]);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
   }
-  function back() { 
-    setHistory(prev => [...prev.slice(0, prev.length - 1)])
-   }
-//return { mode, transition };
-  return { mode: history[history.length -1],
-           transition: transition, 
-           back: back
-          };
+  function back() {
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    }
+  }
+  //return { mode, transition };
+  return {
+    mode: history[history.length - 1],
+    transition: transition,
+    back: back,
+  };
 }
